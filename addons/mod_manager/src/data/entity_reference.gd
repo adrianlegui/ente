e600270386939a3318ebd4059fad7d3665ab8b46
adrian leguizamon <adrian.luis.leguizamon@gmail.com>
2025-01-18 21:31:57 -0300

    refactor(EntityReference): simplifico la clase usando método de ModManager

--- addons/mod_manager/src/data/entity_reference.gd
+++ addons/mod_manager/src/data/entity_reference.gd
@@ -6,18 +6,5 @@ class_name EntityReference extends Data
 @export var entity_id: StringName = &""
 
 
-var _reference: Entity
-
-
-func _ready() -> void:
-	add_to_group(GameEvents.GROUP)
-
-
 func get_reference() -> Entity:
-	return _reference
-
-
-func _on_game_event_all_entities_added() -> void:
-	if entity_id.is_empty():
-		push_error("entity_id esta vacio")
-	_reference = get_node_or_null("/root/" + entity_id)
+	return MOD_MANAGER.get_entity(entity_id)

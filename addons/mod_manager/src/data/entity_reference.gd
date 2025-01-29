25a711225774733f7068adb59d04e3e8524359a2
adrian leguizamon <adrian.luis.leguizamon@gmail.com>
2025-01-29 01:41:24 -0300

    docs(EntityReference): agrego descripción a la clase

--- addons/mod_manager/src/data/entity_reference.gd
+++ addons/mod_manager/src/data/entity_reference.gd
@@ -4,14 +4,17 @@ class_name EntityReference extends Data
 
 @export var _entity_id: StringName = &""
 
+## Obtiene una referencia a la entidad configurada.
 func get_reference() -> Entity:
 	return MOD_MANAGER.get_entity(_entity_id)
 
 
+## Obtiene el identificador de la entidad
 func get_entity_id() -> String:
 	return _entity_id
 
 
+## Configura el identificador de la entidad
 func set_entity_id(entity_id: String) -> void:
 	_entity_id = entity_id
 

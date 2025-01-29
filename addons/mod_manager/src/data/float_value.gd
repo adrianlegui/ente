45eaaba3c83b3d7a17dfc2fa4421a13c4e86d46c
adrian leguizamon <adrian.luis.leguizamon@gmail.com>
2025-01-29 01:39:41 -0300

    docs(FloatValue): agrego descripción a la clase

--- addons/mod_manager/src/data/float_value.gd
+++ addons/mod_manager/src/data/float_value.gd
@@ -1,31 +1,39 @@
 @icon("res://addons/mod_manager/src/icons/float_value.svg")
 class_name FloatValue extends Data
-## Usado para guardar valores float.
-##
-## @experimental
+## Usado para guardar valores [code]float[/code].
 
-## Valor base.
+# Valor base.
 @export var _base: float = 0.0
 
-## Modificador del valor.
+# Modificador del valor.
 var _mod: float = 0.0
 
+## Configura el valor base.
 func set_base_value(value: float) -> void:
 	_base = value
 
 
+## obtiene el valor base.
 func get_base_value() -> float:
 	return _base
 
 
+## Agrega [param value] al valor base.
+func add_base_value(value: float) -> void:
+	_base += value
+
+
+## Configura el modificador del valor.
 func set_mod_value(value: float) -> void:
 	_mod = value
 
 
+## Obtiene el modificador del valor:
 func get_mod_value() -> float:
 	return _mod
 
 
+## Agrega [param value] al modificador del valor.
 func add_mod_value(value: float) -> void:
 	_mod += value
 

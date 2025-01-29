d74aef51aaa0311343b7da5044d064b1ee90fb5b
adrian leguizamon <adrian.luis.leguizamon@gmail.com>
2025-01-29 01:38:39 -0300

    docs(DataList): agrego descripción a la clase

--- addons/mod_manager/src/data/data_list.gd
+++ addons/mod_manager/src/data/data_list.gd
@@ -1,6 +1,7 @@
 class_name DataList extends Data
+## Lista de nodos [Data].
 
-
+## Configura los nodos [Data] de la lista.
 func set_data(data: Dictionary) -> void:
 	for id in data:
 		var d: Data = get_node_or_null(id)
@@ -13,6 +14,8 @@ func set_data(data: Dictionary) -> void:
 			add_data(data_node)
 
 
+## Obtiene un [Dictionary] con la información persistente de todos los nodos
+## [Data] de la lista.
 func get_data() -> Dictionary:
 	var data: Dictionary = {}
 	for child in get_children():
@@ -22,14 +25,18 @@ func get_data() -> Dictionary:
 	return data
 
 
+## Obtiene el [Array] de nodos [Data].
 func get_data_nodes() -> Array[Data]:
 	return get_children() as Array[Data]
 
 
+## Agrega un nodo [Data] a la lista.
 func add_data(data_node: Data) -> void:
 	var force_readable_name: bool = true
 	add_child(data_node, force_readable_name)
 
 
+## Obtiene un nodo [Data] por su identificador.[br]
+## [param id]: nombre del nodo.
 func get_data_by_name(id: String) -> Data:
 	return get_node_or_null(id)

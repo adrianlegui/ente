ef4e3ccbb54241f13536596bce68c9982c6ba152
adrian leguizamon <adrian.luis.leguizamon@gmail.com>
2025-01-29 01:37:50 -0300

    docs(BoolValue): agrego descripción a la clase

--- addons/mod_manager/src/data/bool_value.gd
+++ addons/mod_manager/src/data/bool_value.gd
@@ -1,15 +1,19 @@
 @icon("res://addons/mod_manager/src/icons/bool_value.svg")
 class_name BoolValue extends Data
-## @experimental
+## Configura y administra variable [code]bool[/code],
 
+## Se emite cuando el valor default cambia.
 signal default_changed
-signal bloker_added(entity: Entity)
-signal bloker_removed(entity: Entity)
+## Se emite cuando se agrega un nodo bloqueador.
+signal bloker_added(blocker: Data)
+## Se emite cuando se quita un nodo bloqueador
+signal bloker_removed(blocker: Data)
 
 @export var _default: bool = true
 
 var _blockers: PackedStringArray = []
 
+## Configura valor default.
 func set_default(value: bool, force: bool = false) -> void:
 	if _default == value:
 		return
@@ -24,35 +28,41 @@ func set_default(value: bool, force: bool = false) -> void:
 		default_changed.emit()
 
 
+## Obtine valor default.
 func get_default() -> bool:
 	return _default
 
-
+## Regresa [code]true[/code] si [BoolValue] es [code]true[/code].
 func is_true() -> bool:
 	return _default if _blockers.size() == 0 else false
 
 
+## Regresa [code]true[/code] si no hay nodos bloqueando [BoolValue].
 func can_be_true() -> bool:
 	return _blockers.size() == 0
 
 
-func add_blocker(entity: Entity) -> void:
-	var id: String = entity.name
+## Agrega un nodo que bloquea al [BoolValue].
+func add_blocker(blocker: Data) -> void:
+	var id: String = blocker.name
 	if not _blockers.has(id):
 		_blockers.append(id)
-		bloker_added.emit(entity)
+		bloker_added.emit(blocker)
 
 
-func remove_blocker(entity: Entity) -> void:
-	var id: String = entity.name
+## Quita un nodo que bloquea al [BoolValue].
+func remove_blocker(blocker: Data) -> void:
+	var id: String = blocker.name
 	var idx: int = _blockers.find(id)
 	if idx != -1:
 		_blockers.remove_at(idx)
-		bloker_removed.emit(entity)
+		bloker_removed.emit(blocker)
 
 
-func has_blocker(entity: Entity) -> bool:
-	return _blockers.has(entity.name)
+## Regresa [code]true[/code] si el nodo [param blocker] esta bloqueando el
+## [BoolValue].
+func has_blocker(blocker: Data) -> bool:
+	return _blockers.has(blocker.name)
 
 
 func _get_persistent_keys() -> PackedStringArray:

0b7f6b378ab6686061dc3ff70ef079438278ded6
adrian leguizamon <adrian.luis.leguizamon@gmail.com>
2025-01-29 01:39:16 -0300

    docs(IntValue): agrego descripción a la clase

--- addons/mod_manager/src/data/int_value.gd
+++ addons/mod_manager/src/data/int_value.gd
@@ -1,31 +1,39 @@
 @icon("res://addons/mod_manager/src/icons/int_value.svg")
 class_name IntValue extends Data
-## Usado para guardar valores [code]int[\code].
-##
-## @experimental
+## Usado para guardar valores [code]int[/code].
 
-## Valor base.
+# Valor base.
 @export var _base: int = 0
 
-## Modificador del valor.
+# Modificador del valor.
 var _mod: int = 0
 
+## Configura el valor base.
 func set_base_value(value: int) -> void:
 	_base = value
 
 
+## Obtiene el valor base.
 func get_base_value() -> int:
 	return _base
 
 
+## Agrega [param value] al valor base.
+func add_base_value(value: int) -> void:
+	_base += value
+
+
+## Configura modificacion del valor.
 func set_mod_value(value: int) -> void:
 	_mod = value
 
 
+## Obtiene modificación del valor.
 func get_mod_value() -> int:
 	return _mod
 
 
+## Agrega [param value] al modificador.
 func add_mod_value(value: int) -> void:
 	_mod += value
 

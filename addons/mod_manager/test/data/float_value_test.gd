001146139bb48f7b79cc0c538ab5d7b0ae0125a5
adrian leguizamon <adrian.luis.leguizamon@gmail.com>
2025-01-30 01:25:04 -0300

    test(FloatValue): agrego pruebas unitarias faltantes

--- addons/mod_manager/test/data/float_value_test.gd
+++ addons/mod_manager/test/data/float_value_test.gd
@@ -6,23 +6,59 @@ extends GdUnitTestSuite
 
 # TestSuite generated from
 const __source = 'res://addons/mod_manager/src/data/float_value.gd'
-
+const APPROX: float = 0.0001
 
 func test_get_current_value() -> void:
+	var float_value: FloatValue = auto_free(FloatValue.new()) as FloatValue
 	var base: float = 1.2
 	var mod: float = 1.8
-	var float_value: FloatValue = FloatValue.new()
-	float_value.base = base
-	float_value.mod = mod
+	float_value.add_base_value(base)
+	float_value.add_mod_value(mod)
 	var result: float = float_value.get_current_value()
-	assert_float(result).is_equal_approx(base + mod, 0.001)
-	float_value.queue_free()
+	assert_float(result).is_equal_approx(base + mod, APPROX)
 
 
 func test__get_persistent_keys() -> void:
-	var base: String = "base"
-	var mod: String = "mod"
-	var float_value: FloatValue = FloatValue.new()
+	var float_value: FloatValue = auto_free(FloatValue.new()) as FloatValue
 	var vars: PackedStringArray = float_value._get_persistent_keys()
-	assert_array(vars).contains_same([base, mod])
-	float_value.queue_free()
+	assert_array(vars).contains_same(["_base", "_mod"])
+
+
+func test_add_mod_value() -> void:
+	var float_value: FloatValue = auto_free(FloatValue.new()) as FloatValue
+	var mod: float = 83.0
+	float_value.add_mod_value(mod)
+	float_value.add_mod_value(mod)
+	assert_float(float_value.get_mod_value()).is_equal_approx(mod * 2, APPROX)
+
+
+func test_get_mod_value() -> void:
+	var float_value: FloatValue = auto_free(FloatValue.new()) as FloatValue
+	assert_float(float_value.get_mod_value()).is_equal_approx(0.0, APPROX)
+
+
+func test_set_mod_value() -> void:
+	var float_value: FloatValue = auto_free(FloatValue.new()) as FloatValue
+	var mod: float = 23.0
+	float_value.set_mod_value(mod)
+	assert_float(float_value.get_mod_value()).is_equal_approx(mod, APPROX)
+
+
+func test_add_base_value() -> void:
+	var float_value: FloatValue = auto_free(FloatValue.new()) as FloatValue
+	var base:float = 9.0
+	float_value.add_base_value(base)
+	float_value.add_base_value(base)
+	assert_float(float_value.get_base_value()).is_equal_approx(base * 2, APPROX)
+
+
+func test_get_base_value() -> void:
+	var float_value: FloatValue = auto_free(FloatValue.new()) as FloatValue
+	assert_float(float_value.get_base_value()).is_equal_approx(0.0, APPROX)
+
+
+func test_set_base_value() -> void:
+	var float_value: FloatValue = auto_free(FloatValue.new()) as FloatValue
+	var base: float = 9.0
+	float_value.set_base_value(base)
+	assert_float(float_value.get_base_value()).is_equal_approx(base, APPROX)

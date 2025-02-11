12ae3903492c3140b53e15907bb441e34f11de7b
adrian leguizamon <adrian.luis.leguizamon@gmail.com>
2025-02-11 14:57:25 -0300

    refactor(ModManager): extraigo codigo para la fusión de diccionarios y lo convierto en clase DictionaryMerger

---
+++ addons/mod_manager/src/dictionary_merger/dictionary_merger.gd
@@ -0,0 +1,11 @@
+class_name DictionaryMerger extends RefCounted
+## Fusiona [Dictionary].
+
+## Fusiona [param dictionary_b] con [param dictionary_a].
+static func merge(dict_a: Dictionary, dict_b: Dictionary) -> Dictionary:
+	for key in dict_b:
+		if dict_a.has(key) and typeof(dict_a[key]) == TYPE_DICTIONARY:
+			merge(dict_a[key], dict_b[key])
+		else:
+			dict_a[key] = dict_b[key]
+	return dict_a
---
+++ addons/mod_manager/test/dictionary_merger_test/dictionary_merger_test.gd
@@ -0,0 +1,51 @@
+# GdUnit generated TestSuite
+class_name DictionaryMergerTest
+extends GdUnitTestSuite
+@warning_ignore('unused_parameter')
+@warning_ignore('return_value_discarded')
+
+# TestSuite generated from
+const __source = (
+	'res://addons/mod_manager/src/dictionary_merger/dictionary_merger.gd'
+)
+
+
+func test_merge() -> void:
+	var key: String = "key"
+	var key_overwrite: String = "key_overwrite"
+	var valor: String = "original"
+	var dict_key: = "dict_key"
+	var dict_a: Dictionary = {
+		key: valor,
+		key_overwrite: valor,
+		dict_key: {
+			key: valor,
+			key_overwrite: valor
+		}
+	}
+
+	var new_key: String = "new_key"
+	var new_valor: String = "no es original"
+	var dict_b: Dictionary = {
+		key_overwrite : new_valor,
+		new_key : valor,
+		dict_key: {
+			key_overwrite: new_valor,
+			new_key: valor
+		}
+	}
+
+	var result: Dictionary = DictionaryMerger.merge(dict_a, dict_b)
+	assert_object(result).is_same(dict_a)
+	assert_str(result[key]).is_equal(valor)
+	assert_str(result[new_key]).is_equal(valor)
+	assert_str(result[key_overwrite]).is_equal(new_valor)
+	assert_str(
+		result[dict_key][key]
+	).is_equal(valor)
+	assert_str(
+		result[dict_key][key_overwrite]
+	).is_equal(new_valor)
+	assert_str(
+		result[dict_key][new_key]
+	).is_equal(valor)

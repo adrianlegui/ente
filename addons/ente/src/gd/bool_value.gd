d406ffef5a032179e35d756759104b49cc3d1b03
adrian leguizamon <adrian.luis.leguizamon@gmail.com>
2025-03-25 15:21:12 -0300

    refactor(bool_value.gd): bloqueadores pasan a ser String, antes eran rutas de nodos

--- addons/ente/src/gd/bool_value.gd
+++ addons/ente/src/gd/bool_value.gd
@@ -3,11 +3,7 @@ class_name BoolValue extends Data
 ## Configura y administra variable [code]bool[/code],
 
 ## Se emite cuando el valor default cambia.
-signal default_changed
-## Se emite cuando se agrega un nodo bloqueador.
-signal bloker_added(blocker: Data)
-## Se emite cuando se quita un nodo bloqueador
-signal bloker_removed(blocker: Data)
+signal value_changed
 
 ## Valor predeteminado
 @export var _default: bool = true
@@ -28,10 +24,10 @@ func set_default(value: bool, force: bool = false) -> void:
 		_default = value and can_be_true()
 
 	if before != _default:
-		default_changed.emit()
+		value_changed.emit()
 
 
-## Obtine valor default.
+## Obtiene valor default.
 func get_default() -> bool:
 	return _default
 
@@ -41,54 +37,42 @@ func is_true() -> bool:
 	return _default if _blockers.size() == 0 else false
 
 
-## Regresa [code]true[/code] si no hay nodos bloqueando [BoolValue].
+## Regresa [code]true[/code] si no hay bloqueadores bloqueando [BoolValue].
 func can_be_true() -> bool:
 	return _blockers.size() == 0
 
 
-## Agrega un nodo que bloquea al [BoolValue].
-func add_blocker(blocker: Node) -> void:
-	var id: String = _get_node_id(blocker)
+## Agrega un bloqueador que bloquea al [BoolValue].
+func add_blocker(blocker: String) -> void:
+	if blocker.is_empty():
+		push_error("blocker está vacio")
+		return
+	if not _blockers.has(blocker):
+		_blockers.append(blocker)
+		value_changed.emit()
 
-	if id.is_empty():
-		push_error("no se pudo agregar blocker: %s" % blocker.name)
+
+## Quita un bloqueador que bloquea al [BoolValue].
+func remove_blocker(blocker: String) -> void:
+	if blocker.is_empty():
+		push_error("no se pudo quitar blocker porque esta vacio")
 		return
 
-	if not _blockers.has(id):
-		_blockers.append(id)
-		bloker_added.emit(blocker)
-
-
-## Quita un nodo que bloquea al [BoolValue].
-func remove_blocker(blocker: Node) -> void:
-	var id: String = _get_node_id(blocker)
-
-	if id.is_empty():
-		push_error("no se pudo quitar blocker: %s" % blocker.name)
-		return
-
-	var idx: int = _blockers.find(id)
+	var idx: int = _blockers.find(blocker)
 	if idx != -1:
 		_blockers.remove_at(idx)
-		bloker_removed.emit(blocker)
+		value_changed.emit()
+	else:
+		push_error("%s no es un blocker" % blocker)
 
 
-## Regresa [code]true[/code] si el nodo [param blocker] esta bloqueando el
+## Regresa [code]true[/code] si [param blocker] esta bloqueando el
 ## [BoolValue].
-func has_blocker(blocker: Node) -> bool:
-	return _blockers.has(_get_node_id(blocker))
+func has_blocker(blocker: String) -> bool:
+	return _blockers.has(blocker)
 
 
 func _get_persistent_properties() -> PackedStringArray:
 	var keys: PackedStringArray = super._get_persistent_properties()
 	keys.append_array(["_blockers", "_default"])
 	return keys
-
-
-func _get_node_id(node: Node) -> String:
-	var id: String = node.get_path()
-	if id.is_empty():
-		push_error(
-			"no se pudo obtener id para el nodo %s porque no esta dentro del SceneTree" % node.name
-		)
-	return id
--- addons/ente/test/gd/bool_value_test.gd
+++ addons/ente/test/gd/bool_value_test.gd
@@ -14,10 +14,7 @@ func test_set_default() -> void:
 	bool_value.set_default(false)
 	assert_bool(bool_value.get_default()).is_false()
 
-	# no cambia
-	var entity := auto_free(Data.new()) as Data
-	add_child(entity)
-	bool_value.add_blocker(entity)
+	bool_value.add_blocker("entity")
 	bool_value.set_default(true)
 	assert_bool(bool_value.get_default()).is_false()
 
@@ -37,59 +34,39 @@ func test_get_default() -> void:
 func test_is_true() -> void:
 	var bool_value: BoolValue = auto_free(BoolValue.new()) as BoolValue
 	assert_bool(bool_value.is_true()).is_true()
-	var blocker: Data = auto_free(Data.new()) as Data
-	add_child(blocker)
-	bool_value.add_blocker(blocker)
+	bool_value.add_blocker("blocker")
 	assert_bool(bool_value.is_true()).is_false()
 
 
 func test_can_be_true() -> void:
 	var bool_value: BoolValue = auto_free(BoolValue.new()) as BoolValue
 	assert_bool(bool_value.can_be_true()).is_true()
-	var blocker: Data = auto_free(Data.new()) as Data
-	add_child(blocker)
-	bool_value.add_blocker(blocker)
+	bool_value.add_blocker("blocker")
 	assert_bool(bool_value.can_be_true()).is_false()
 
 
 func test_add_blocker() -> void:
 	var bool_value: BoolValue = auto_free(BoolValue.new()) as BoolValue
-	var blocker: Data = auto_free(Data.new()) as Data
-	add_child(blocker)
-	bool_value.add_blocker(blocker)
-	assert_bool(bool_value.has_blocker(blocker)).is_true()
+	bool_value.add_blocker("blocker")
+	assert_bool(bool_value.has_blocker("blocker")).is_true()
 
 
 func test_remove_blocker() -> void:
 	var bool_value: BoolValue = auto_free(BoolValue.new()) as BoolValue
-	var blocker: Data = auto_free(Data.new()) as Data
-	add_child(blocker)
-	bool_value.add_blocker(blocker)
-	assert_bool(bool_value.has_blocker(blocker)).is_true()
-	bool_value.remove_blocker(blocker)
-	assert_bool(bool_value.has_blocker(blocker)).is_false()
+	bool_value.add_blocker("blocker")
+	assert_bool(bool_value.has_blocker("blocker")).is_true()
+	bool_value.remove_blocker("blocker")
+	assert_bool(bool_value.has_blocker("blocker")).is_false()
 
 
 func test_has_blocker() -> void:
 	var bool_value: BoolValue = auto_free(BoolValue.new()) as BoolValue
-	var blocker_0: Data = auto_free(Data.new()) as Data
-	add_child(blocker_0)
-	assert_bool(bool_value.has_blocker(blocker_0)).is_false()
-	var blocker_1: Data = auto_free(Data.new()) as Data
-	add_child(blocker_1)
-	bool_value.add_blocker(blocker_1)
-	assert_bool(bool_value.has_blocker(blocker_1)).is_true()
+	assert_bool(bool_value.has_blocker("blocker_0")).is_false()
+	bool_value.add_blocker("blocker_1")
+	assert_bool(bool_value.has_blocker("blocker_1")).is_true()
 
 
 func test__get_persistent_properties() -> void:
 	var bool_value: BoolValue = auto_free(BoolValue.new()) as BoolValue
 	var keys: PackedStringArray = ["_blockers", "_default"]
 	assert_array(bool_value._get_persistent_properties()).contains(keys)
-
-
-func test__get_node_id() -> void:
-	var bool_value: BoolValue = auto_free(BoolValue.new()) as BoolValue
-	var node := auto_free(Node.new()) as Node
-	node.name = "Nodo"
-	add_child(node)
-	assert_str(bool_value._get_node_id(node)).is_equal(str(node.get_path()))
